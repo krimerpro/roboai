3fbc50093089382fed997dc62b4eaa24b64d5ef4
varanustoken <varanustoken@gmail.com>
2023-02-14 07:25:13 +0700

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,7 +20,7 @@
                 </h1>
                 <p class="head-p">A brighter future for all. We will make a difference.</p>
                 <div class="btn-wrap">
-                        <a href="https://poocoin.app/tokens/" class="btn btn-default main_btn"><span>POOCOIN CHART</span></a>
+                        <a href="https://poocoin.app/tokens/0xDb5D686A3B634Df1F6500101C235F94CB113E0dC" class="btn btn-default main_btn"><span>POOCOIN CHART</span></a>
                         <a href="https://t.me/RoboAIBsc" class="btn btn-default main_btn"><span>TELEGRAM GROUP</span></a>
 
                 </div>
